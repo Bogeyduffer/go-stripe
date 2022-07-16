d40213617884b620b588985da058d0713a1f83c1
Robert Nicholson <bob@busapp.com>
2022-07-16 18:25:56 -0500

    After lecture 96.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -321,3 +321,11 @@ func (app *application) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	app.Session.Put(r.Context(), "userID", id)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+// Logout displays the login page
+func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
+	app.Session.Destroy(r.Context())
+	app.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
